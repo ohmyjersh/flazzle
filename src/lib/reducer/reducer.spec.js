0dacca70cef9e82b582d1f128b8636240e6ee488
JJ <jjarmain@gmail.com>
2018-06-04 21:30:01 -0700

    make test pass

--- src/lib/reducer/reducer.spec.js
+++ src/lib/reducer/reducer.spec.js
@@ -5,7 +5,7 @@ let initialFlags = {
     flag1: false
 }
 
-let actionType = 'SUPER_SECRET_ACTION_TO_UPDATE_FLAGS_FOR_FLIZZLE_PLEASE_BE_UNIQUE_SO_NO_OTHER_REDUCER_USES_IT_69';
+let actionType = 'SUPER_SECRET_ACTION_TO_UPDATE_FLAGS_FOR_FLIZZLE_PLEASE_BE_UNIQUE_SO_NO_OTHER_REDUCER_USES_IT_8675309';
 
 test('flags are set at initialState', () => {
     let state = flazzleReducer(initialFlags)(undefined, {type:'lol'})
